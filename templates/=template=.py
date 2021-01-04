e419de62887c0005d0d5b9353839fa4025003477
Mike Christof <mhristof@gmail.com>
2021-01-04 10:00:10 +0000

    added pre-commit templates and updated py

--- templates/=template=.py
+++ templates/=template=.py
@@ -1,15 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © %YEAR% %USER% <%MAIL%>
-#
-# Distributed under terms of the %LICENSE% license.
 
 """
 %HERE%
 """
-
-import logging
-
-LOGGER = logging.getLogger('%FILE%')
